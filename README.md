9463ee6374704288ef71060efe6ccbcb630e6a8b
Yusuke Shinyama <yusuke@shinyama.jp>
2014-08-09 11:23:45 +0900

    Added: parse_c

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Data We Used
 ------------
 
  * Pick the top 100 repositories (in terms of Stars) that are labelled as: C, Java and Python.
+   (via GitHub Search API, cf. https://developer.github.com/v3/search/)
  * List all the files: (Caveats: We excluded non-ASCII filenames.)
    * C: 94637 files
    * Java: 33756 files
@@ -15,3 +16,23 @@ Data We Used
    * C: 153MB in total
    * Java: 57MB in total
    * Python: 95MB in total
+
+Skewed Data
+-----------
+
+ * Many files in C are from the Linux kernel.
+   (3611 files from torvalds/linux, 3336 files from raspberrypi/linux)
+ * Many files in Java are from JetBrains IntelliJ.
+   (3599 files from JetBrains/intellij-community)
+ * Many files in Python are from Appscale.
+   (2417 files from AppScale/appscale)
+
+Language Parsing
+----------------
+
+ * We used ANTLR4 for C and Java (cf. http://antlr.org), 
+   and ast module for Python (cf. https://docs.python.org/2/library/ast.html).
+ * C Caveats: ANTLR4 cannot handle preprocessor directives,
+   so we stripped out #defines and #includes from the code.
+   After all, we're interested in a source code for human readers, not for compilers.
+ * Python Caveats: Mixture of Python 2 and 3 code.
---
+++ tools/parse_c.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import sys
+import os
+import zipfile
+import subprocess
+
+os.environ['CLASSPATH'] = '/usr/share/java/antlr-complete.jar:.'
+
+def fakecpp(lines):
+    decl = False
+    for line in lines:
+        if decl or line.lstrip().startswith('#'):
+            decl = line.rstrip().endswith('\\')
+        else:
+            yield line
+    return
+
+def parse_c(data):
+    lines = data.splitlines()
+    lines = fakecpp(lines)
+    proc = subprocess.Popen(['grun','C','compilationUnit','-tree'],
+                            stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    for line in lines:
+        proc.stdin.write(line)
+    proc.stdin.close()
+    data = proc.stdout.read()
+    proc.stdout.close()
+    return data
+
+def main(argv):
+    args = argv[1:]
+    outpath = args.pop(0)
+    out = zipfile.ZipFile(outpath, 'w', compression=zipfile.ZIP_DEFLATED)
+    for path in args:
+        zf = zipfile.ZipFile(path)
+        for name in zf.namelist():
+            sys.stderr.write('parsing: %r\n' % name)
+            sys.stderr.flush()
+            data = zf.read(name)
+            tree = parse_c(data)
+            out.writestr(name, tree)
+        zf.close()
+    out.close()
+    return 0
+
+if __name__ == '__main__': sys.exit(main(sys.argv))
