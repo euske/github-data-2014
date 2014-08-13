cf0292671a4968c900a8fa28cbd6143c5d2f8023
Yusuke Shinyama <yusuke@shinyama.jp>
2014-08-13 11:57:30 +0900

    Updated: traverse_c

--- tools/traverse_c.py
+++ tools/traverse_c.py
@@ -17,6 +17,12 @@ def walk(x):
                 yield y
     return
 
+def issingle(x):
+    while islist(x):
+        if len(x) != 2: return None
+        x = x[1]
+    return x
+
 def traverse_c(tree):
     if not islist(tree): return
     if head(tree) == '.functionDefinition':
@@ -32,7 +38,6 @@ def traverse_c(tree):
             else:
                 traverse_c(t)
     elif head(tree) == '.declaration':
-        #pp(tree); print
         for t in tail(tree):
             if head(t) == '.initDeclaratorList':
                 r = []
@@ -42,6 +47,21 @@ def traverse_c(tree):
                         print 'vardecl', v
             else:
                 traverse_c(t)
+    elif head(tree) in ('.initializer', '.expression'):
+        for t in walk(tail(tree)):
+            if head(t) == '.postfixExpression':
+                if (3 <= len(t) and
+                    issingle(t[1]) and
+                    issym(t[2]) == '.('):
+                    v = issingle(t[1])
+                    r = [v]
+                    for x in t[3:-1]:
+                        v = issingle(x)
+                        if issym(v):
+                            r.append(v)
+                        else:
+                            r.append('*')
+                    print 'expr', ' '.join(r)
     else:
         for t in tail(tree):
             traverse_c(t)
