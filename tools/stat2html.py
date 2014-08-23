165aea74b20f4cd0f7eac607db91d395d4d05895
Yusuke Shinyama <yusuke@shinyama.jp>
2014-08-24 01:31:00 +0900

    Style tweaks.

--- tools/stat2html.py
+++ tools/stat2html.py
@@ -16,14 +16,13 @@ def listwords(tables,n=10):
     print '<table class=list>'
     print '<tr><th>#</th>'
     for (col,h) in enumerate(HEADERS[:len(tables)]):
-        print '<th class=col_%d colspan=2>%s</th>' % (col,h)
+        print '<th class=col_%d>%s</th>' % (col,h)
     print '</tr>'
     i = 1
     for row in zip(*tables)[:n]:
         r = [('<td class=order>%d.</td>' % i)]
         for (col,(w,n)) in enumerate(row):
-            r.append('<td class="col_%d word"><code>%s</code></td>' % (col, q(w)))
-            r.append('<td class="col_%d count">%d</td>' % (col, n))
+            r.append('<td class="col_%d word"><a href="javascript:void(0);" title="%d">%s</a></td>' % (col, n, q(w)))
         print '<tr>%s</tr>' % ''.join(r)
         i += 1
     print '</table>'
@@ -32,8 +31,8 @@ def listwords(tables,n=10):
 def listphrases(verb,assoc,n=10):
     print '<table class=list>'
     print '<tr><th>#</th>'
-    print '<th class=col_phrase colspan=2>Verb+Noun</th>'
-    print '<th class=col_related colspan=2>Related Nouns</th>'
+    print '<th class=col_phrase>Verb+Noun</th>'
+    print '<th class=col_related>Related Nouns</th>'
     print '</tr>'
     i = 1
     COLS = ('phrase', 'related')
@@ -42,11 +41,10 @@ def listphrases(verb,assoc,n=10):
         for (col,(w,n)) in zip(COLS,row):
             (w1,_,w2) = w.partition(' ')
             if col == 'phrase':
-                w = '%s %s.' % (w1[0].upper()+w1[1:], w2)
+                w = '%s %s.' % (w1[0].upper()+w1[1:], w2[0].upper()+w2[1:])
             else:
                 w = '%s, %s' % (w1,w2)
-            r.append('<td class="col_%s word"><code>%s</code></td>' % (col,q(w)))
-            r.append('<td class="col_%s count">%d</td>' % (col, n))
+            r.append('<td class="col_%s word"><a href="javascript:void(0);" title="%d">%s</a></td>' % (col, n, q(w)))
         print '<tr>%s</tr>' % ''.join(r)
         i += 1
     print '</table>'
@@ -76,36 +74,48 @@ def fmtpairs(words, fmt):
 def main(argv):
     print '''<html><head>
 <style><!--
-table.list { border: 1px gray solid; }
-div.category { font-size: 100%; font-weight: bold; padding-bottom: 0.5em; }
+table.list { border: 2px gray solid; border-spacing: 0px; border-collapse: collapse; }
+.category { font-size: 100%; font-weight: bold; padding-bottom: 0.5em; }
+.nav { font-size: 80%; }    
 .col_0 { background: #ffddff; }
 .col_1 { background: #eeeeee; }
 .col_2 { background: #eeeeee; }
 .col_phrase { background: #ddffdd; }
 .col_related { background: #ddddff; }
-th { border-bottom: 2px gray solid; border-right: 2px gray solid; }
-td { padding: 0.2em; }
-td.order { border-right: 2px gray solid; text-align: right; }
-td.count { border-right: 2px gray solid; text-align: right; padding-right: 0.5em; }
-td.word { font-weight: bold; }
+h1 { color: white; background: #442222; }
+h2 { border-bottom: 4px red solid; }
+th { border: 2px gray solid; }
+td.tab { padding: 1em; }
+td.order { border: 2px gray solid; text-align: right; }
+td.word { border: 2px gray solid; padding: 0.2em; font-weight: bold; }
 --></style>
 </head><body>'''
+    print '<h1>Result: Quest for True Names</h1>'
+    print '<p><a href="https://github.com/euske/github-data-2014">Project Repository</a>'
+    print
     for path in argv[1:]:
         d = readlist(path)
-        print '<h1>Top Names for %s</h1>' % path
+        if '-java.' in path:
+            name = 'Java'
+        elif '-c.' in path:
+            name = 'C'
+        elif '-py.' in path:
+            name = 'Python'
+        print '<h2>Top Names for %s</h2>' % name
+        print '<div class=nav>(Hover over a word to see the frequency.)</div>'
         print '<table><tr>'
-        print '<td><div class=category>Variable (noun)</div>'
+        print '<td class=tab><div class=category>Variable (<em>noun</em>)</div>'
         listwords((d['var_word'], d['var_prefix'], d['var_suffix'], d['var_bigram']))
         print '</td>'
-        print '<td><div class=category>Type/Class</div>'
+        print '<td class=tab><div class=category>Type/Class</div>'
         listwords((d['type_word'], d['type_prefix'], d['type_suffix'], d['type_bigram']))
         print '</td>'
         print '</tr></table>'
         print '<table><tr>'
-        print '<td><div class=category>Function/Method (verb)</div>'
+        print '<td class=tab><div class=category>Function/Method (<em>verb</em>)</div>'
         listwords((d['func_word'], d['func_prefix'], d['func_suffix'], d['func_bigram']))
         print '</td>'
-        print '<td><div class=category>Combination</div>'
+        print '<td class=tab><div class=category>Combination</div>'
         listphrases(d['func_verb'], d['func_assoc'])
         print '</td>'
         print '</tr></table>'
